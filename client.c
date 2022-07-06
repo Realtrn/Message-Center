bc306b8ad73010021274282e4b62d064743f0143
root <root@Hvn_Trungnd.humaxdigital.com>
2022-07-06 10:03:58 +0700

    commit chuyen branch

--- client.c
+++ client.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <signal.h>
-
 #include <unistd.h>
-
 #include <sys/types.h>
-
 #include <sys/socket.h>
-
 #include <netinet/in.h>
-
 #include <arpa/inet.h>
-
 #include <pthread.h>
-
 #include <termios.h>
-
 #define LENGTH 2048
 
 
